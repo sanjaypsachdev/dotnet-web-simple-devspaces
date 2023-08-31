ac5ebb6f110c11ab8cdc745badba9b4469f81366
Sanjay Sachdev <ssachdev.pros@gmail.com>
2023-08-31 12:30:46 +0000

    Test

--- Program.cs
+++ Program.cs
@@ -3,34 +3,102 @@ using Microsoft.Extensions.Logging.Console;
 using OpenTelemetry;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+//var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// if (!app.Environment.IsDevelopment())
+// {
+//     app.UseExceptionHandler("/Home/Error");
+//     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//     app.UseHsts();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthorization();
+
+// app.Logger.LogInformation("Adding Routes");
+
+// app.MapControllerRoute(
+//     name: "default",
+//     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+builder.Services.AddMemoryCache();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddControllers();
+
+///Nlog configuration
+// builder.Services.AddSingleton<INavClient, NavClient>();
+
+// builder.Services.AddHttpClient<INavClient, NavClient>()
+//     .ConfigureHttpClient((serviceProvider, httpClient) =>
+//     {
+//         httpClient.Timeout = TimeSpan.FromSeconds(120);
+//         httpClient.DefaultRequestHeaders.Add("TargetApiVersion", "1001");
+//         httpClient.DefaultRequestHeaders.Add("targetapiplatform", "web");
+//         httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+//     })
+//     .SetHandlerLifetime(TimeSpan.FromMinutes(5))    /// Default is 2 mins
+//     .ConfigurePrimaryHttpMessageHandler(x =>
+//         new HttpClientHandler
+//         {
+//             AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+//             UseCookies = false,
+//             AllowAutoRedirect = false,
+//             UseDefaultCredentials = true,
+//         });
+
+//Health Check Configuration
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
+//builder.Services.AddHealthChecks()
+//.AddCheck<SessionCheck>("Session API", tags: new[] { "POD Readiness"})
+//.AddCheck<SRPCheck>("Flight Search API", tags: new[] { "POD Readiness" })
+//.AddCheck<GetPassengerCheck>("Get Passenger API", tags: new[] { "POD Readiness" })
+//.AddCheck<PostPassengerCheck>("Post Passenger API", tags: new[] { "POD Readiness" });
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.MapHealthChecksUI();
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.Logger.LogInformation("Adding Routes");
+app.MapControllers();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+// app.UseEndpoints(endpoints =>
+// {
+//     endpoints.MapHealthChecks("/health",
+//      new HealthCheckOptions()
+//      {
+
+//          ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+//      }
+//     );
+// });
 
 app.Logger.LogInformation("Starting the app");
 
